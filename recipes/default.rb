981b582da3cc9acd2d2a82cf9bb41ad66c417298
Gustavo Mendonca <gustavogmend@gmail.com>
2017-01-31 13:38:49 -0200

    Making luigid to start as a service

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,18 +6,18 @@
 include_recipe 'python'
 
 directory node['luigi']['dir'] do
-  owner   node['luigi']['user']
-  group   node['luigi']['group']
-  mode    '0755'
-  action  :create
+  owner     node['luigi']['user']
+  group     node['luigi']['group']
+  mode      '0755'
+  action    :create
 end
 
 template "#{node['luigi']['dir']}/client.cfg" do
-  source  'client.cfg.erb'
-  owner   node['luigi']['user']
-  group   node['luigi']['group']
-  mode    '0755'
-  action  :create
+  source    'client.cfg.erb'
+  owner     node['luigi']['user']
+  group     node['luigi']['group']
+  mode      '0755'
+  action    :create
   variables({
     :aws_access_key_id => node['aws']['s3-access-key'],
     :aws_secret_access_key => node['aws']['s3-secret-key']
@@ -27,24 +27,43 @@ template "#{node['luigi']['dir']}/client.cfg" do
 end
 
 directory node['luigid']['dir'] do
-  owner   node['luigid']['user']
-  group   node['luigid']['group']
-  mode    '0755'
-  action  :create
+  owner     node['luigid']['user']
+  group     node['luigid']['group']
+  mode      '0755'
+  action    :create
 end
 
 directory node['luigid']['dir']['log'] do
-  owner   node['luigid']['user']
-  group   node['luigid']['group']
-  mode    '0755'
-  action  :create
+  owner     node['luigid']['user']
+  group     node['luigid']['group']
+  mode      '0755'
+  action    :create
 end
 
 directory node['luigid']['dir']['db'] do
-  owner   node['luigid']['user']
-  group   node['luigid']['group']
-  mode    '0755'
-  action  :create
+  owner     node['luigid']['user']
+  group     node['luigid']['group']
+  mode      '0755'
+  action    :create
+end
+
+service "luigid" do
+  supports :restart => true, :start => true, :stop => true, :reload => true
+  action :nothing
+end
+
+template "etc/init.d/luigid" do
+  source    'luigi-server.init.erb'
+  owner     'root'
+  group     'root'
+  mode      '0755'
+  action    :create
+  notifies  :enable, 'service[luigid]'
+  notifies  :start, 'service[luigid]'
+  variables({
+    :luigid_pidfile => node['luigid']['file']['pid']
+    :luigid_logdir => node['luigid']['dir']['log']
+  })
 end
 
 python_pip 'boto'
